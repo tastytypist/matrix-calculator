fd77586986c759f7038e5f39b6f4711ed8f407e5
tastytypist <nathanael310802@gmail.com>
2021-09-28 19:24:38 +0700

    Implement terminal input output operation of polynomial interpolation

--- src/matrixio/MatrixTerminal.java
+++ src/matrixio/MatrixTerminal.java
@@ -7,13 +7,13 @@ public class MatrixTerminal {
 
     static Scanner input = new Scanner(System.in);
 
-    public static MatrixConstructor readMatrixTerminal() {
+    public static Matrix readMatrixTerminal() {
         System.out.print("Input row size: ");
         int row_size = input.nextInt();
         System.out.print("Input column size: ");
         int column_size = input.nextInt();
 
-        MatrixConstructor matrix = MatrixConstructor.createMatrix(row_size, column_size);
+        Matrix matrix = Matrix.createMtr(row_size, column_size);
 
         System.out.println("Input matrix: ");
         for (int i = 0; i < row_size; ++i) {
@@ -26,9 +26,9 @@ public class MatrixTerminal {
         return matrix;
     }
 
-    public static void displayMatrixTerminal(MatrixConstructor matrix) {
-        int row_size = matrix.row_size;
-        int column_size = matrix.column_size;
+    public static void displayMatrixTerminal(Matrix matrix) {
+        int row_size = matrix.row;
+        int column_size = matrix.col;
 
         for (int i = 0; i < row_size; ++i) {
             for (int j = 0; j < column_size; ++j) {
@@ -39,5 +39,29 @@ public class MatrixTerminal {
             }
         }
         System.out.println();
+    }
+
+    public static Object[] readInterpolationTerminal() {
+        System.out.print("Input number point: ");
+        int point_count = input.nextInt();
+        System.out.print("Input x: ");
+        int num = input.nextInt();
+
+        Matrix point_matrix = Matrix.createMtr(point_count, 2);
+
+        System.out.println("Input points: ");
+        for (int i = 0; i < point_count; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                float element = input.nextFloat();
+                point_matrix.contents[i][j] = element;
+            }
+        }
+
+        return new Object[] {point_count, num};
+    }
+
+    public static void displayInterpolationTerminal(String result, String function_result) {
+        System.out.println(result);
+        System.out.println(function_result);
     }
 }
